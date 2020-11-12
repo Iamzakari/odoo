66930cca531ba9788a67f786db7d68a8bb87b88a
zack <irakaz@hotmail.fr>
2020-11-12 20:01:50 +0100

    Module and view are add

---
+++ custom_addons/task_management/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Task Management',
+    'version': '14.0.1.0',
+    'summary': 'Daily tasks management software',
+    'description': 'Employees daily tasks management system',
+    'category': 'Extra Category',
+    'author': 'Dule',
+    'sequence': -100,
+    'licence':'LGPL-3',
+    'depends': [],
+    'data': [
+        'views/task.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
---
+++ custom_addons/task_management/models/task.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models
+
+
+class DailyTask(models.Model):
+    _name = "daily.task"
+    _description = "Employees daily tasks"
+
+    task_name = fields.Char(string='Task Name', required=True)
+    task_description = fields.Text(string='Task Description', required=True)
+    task_owner = fields.Char(string='Task owner')
+    task_type = fields.Selection([
+        ('morning', 'Morning'),
+        ('afternoon', 'Afternoon'),
+        ('home', 'Home'),
+    ], required=True, default='Morning')
+    task_startTime = fields.Date(index=True, help="Starting time")
+    task_endTime = fields.Date(index=True, help="Ending time")
